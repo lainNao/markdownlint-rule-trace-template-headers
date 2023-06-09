41987e08d586eeb43086a553a06f3d997402f533
lainNao <lainNao>
2023-06-10 03:43:58 +0900

    chore: update semantic release settings

---
+++ release.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  release: {
+    branches: ["main"]
+  },
+  plugins: [
+    // DON'T CHANGE THE ORDER OF THE FOLLOWING PLUGINS
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog", // added: for updating the changelog
+    "@semantic-release/npm",
+    "@semantic-release/git", // added: for updating the version in the package.json
+    "@semantic-release/github"
+  ]
+}
