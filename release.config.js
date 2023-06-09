3667a32d2e9d590909a276bb9254a6aa54db6e4b
lainNao <lainNao>
2023-06-10 04:13:24 +0900

    fix: fix auto commit config

--- release.config.js
+++ release.config.js
@@ -1,14 +1,22 @@
 module.exports = {
   release: {
-    branches: ["main"]
+    branches: ["main"],
   },
   plugins: [
     // DON'T CHANGE THE ORDER OF THE FOLLOWING PLUGINS
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog", // added: for updating the changelog
-    "@semantic-release/git", // added: for updating the version in the package.json
-    "@semantic-release/github",
     "@semantic-release/npm",
-  ]
-}
+    "@semantic-release/git", // added: for updating the version in the package.json
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json"],
+        message:
+          "chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
