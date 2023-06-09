3a9fe144b80a36e1a429fccecaa1a13fce81a322
lainNao <lainNao>
2023-06-10 04:58:45 +0900

    fix: fix auto release config

--- release.config.js
+++ release.config.js
@@ -6,17 +6,9 @@ module.exports = {
     // DON'T CHANGE THE ORDER OF THE FOLLOWING PLUGINS
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog", // added: for updating the changelog
+    "@semantic-release/changelog",
     "@semantic-release/npm",
-    "@semantic-release/git", 
-    [
-      "@semantic-release/git", // added: for updating the version in the package.json
-      {
-        assets: ["CHANGELOG.md", "package.json"],
-        message:
-          "chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
+    "@semantic-release/git",
     "@semantic-release/github",
   ],
 };
