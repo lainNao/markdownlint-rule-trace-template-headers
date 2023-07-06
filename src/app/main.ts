549b0d5cf90ff0157f68246928edc72983039314
K・Y <65239667+lainNao@users.noreply.github.com>
2023-07-06 17:25:40 +0900

    chore: typo

--- src/app/main.ts
+++ src/app/main.ts
@@ -4,7 +4,7 @@ import { getRepositoryUrl } from "../helpers/getRepositoryUrl";
 
 export const rule: MarkdownLintRule = {
   names: ["markdownlint-rule-trace-template-headers"],
-  description: "Rule that checks the headers is same to template",
+  description: "Rule that checks the headers are same to template",
   information: new URL(getRepositoryUrl()),
   tags: ["headers", "headings", "template"],
   function: ruleFunction,
